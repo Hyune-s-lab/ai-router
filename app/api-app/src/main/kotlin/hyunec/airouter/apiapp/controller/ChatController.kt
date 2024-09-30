8d0a757219a98de70dc385b296c5de0d83a28054
Hyune-c <choi8608@gmail.com>
2024-09-30 21:19:27 +0900

    [openai chat 연동 by spring-ai] chat mode 선택을 header 로 이관

--- app/api-app/src/main/kotlin/hyunec/airouter/apiapp/controller/ChatController.kt
+++ app/api-app/src/main/kotlin/hyunec/airouter/apiapp/controller/ChatController.kt
@@ -5,9 +5,9 @@ import hyunec.airouter.chatdomain.port.OpenAiChatPort
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.CorePublisher
 
@@ -16,18 +16,18 @@ class ChatController(
     @Qualifier("openAiChatWebClientAdapter") private val webclientChat: OpenAiChatPort,
     @Qualifier("openAiChatSpringAiAdapter") private val springAiChat: OpenAiChatPort,
 ) {
-    private var mode = "webclient"
-
     @PostMapping("/api/chat/completions")
-    fun chat(@RequestBody request: OpenAiChatRequest): ResponseEntity<out CorePublisher<out Any>> {
+    fun chat(
+        @RequestHeader mode: Mode = Mode.SPRING_AI,
+        @RequestBody request: OpenAiChatRequest
+    ): ResponseEntity<out CorePublisher<out Any>> {
         if (request.stream) {
             return ResponseEntity.ok()
                 .contentType(MediaType.TEXT_EVENT_STREAM)
                 .body(
                     when (mode) {
-                        "webclient" -> webclientChat.streamChat(request)
-                        "spring-ai" -> springAiChat.streamChat(request)
-                        else -> throw IllegalArgumentException("Invalid mode: $mode")
+                        Mode.WEBCLIENT -> webclientChat.streamChat(request)
+                        Mode.SPRING_AI -> springAiChat.streamChat(request)
                     }
                 )
         }
@@ -36,21 +36,13 @@ class ChatController(
             .contentType(MediaType.APPLICATION_JSON)
             .body(
                 when (mode) {
-                    "webclient" -> webclientChat.chat(request)
-                    "spring-ai" -> springAiChat.chat(request)
-                    else -> throw IllegalArgumentException("Invalid mode: $mode")
+                    Mode.WEBCLIENT -> webclientChat.chat(request)
+                    Mode.SPRING_AI -> springAiChat.chat(request)
                 }
             )
     }
 
-    @PostMapping("/api/chat/mode")
-    fun setMode(@RequestBody mode: String): String {
-        this.mode = mode
-        return this.mode
-    }
-
-    @GetMapping("/api/chat/mode")
-    fun getMode(): String {
-        return this.mode
+    enum class Mode {
+        WEBCLIENT, SPRING_AI
     }
 }
